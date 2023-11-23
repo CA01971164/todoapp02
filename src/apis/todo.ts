0f6f602684f173b7d10104434b0205c745f50965
01971164 <CA01971164@st.kawahara.ac.jp>
2023-11-23 10:38:24 +0900

    Add todo.ts file

--- src/apis/todo.ts
+++ src/apis/todo.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-export default function App() {
+export default function App(): string {
   const fetchData = async () => {
     const response = await axios.get("http://localhost:3100/todo");
 
     return console.log("response.data");
   };
   fetchData();
-  return <h1>Hello React</h1>;
+  return `<h1>Hello React</h1>`;
 }
+
+// reactコンポーネントでリファクタリングが必要な場合には、教えて
