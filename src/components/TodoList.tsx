5e54b3a0f09f84f2fd0c880cd59e68ab9c765be6
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-11-27 09:26:37 +0900

    fix TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -18,21 +18,40 @@ const TodoList: React.FC<TodoListProps> = ({
     <div>
       <h2>{Head}</h2>
       <ul>
-        {items.map((Todo) =>
-          Todo.done ? (
-            <>
-              <li key={Todo.id}>{Todo.content}</li>
-              <button onClick={() => Onreverse(Todo.id)}>完了リストへ</button>
-              <button onClick={() => onDelete(Todo.id)}>削除</button>
-            </>
-          ) : (
-            <>
-              <li key={Todo.id}>{Todo.content}</li>
-              <button onClick={() => Onreverse(Todo.id)}>未完了リストへ</button>
-              <button onClick={() => onDelete(Todo.id)}>削除</button>
-            </>
-          )
-        )}
+        {items.map((Todo) => {
+          if (Head === "未完了リスト") {
+            return (
+              <>
+                {Todo.done === false && (
+                  <>
+                    <li key={Todo.id}>{Todo.content}</li>
+                    <button onClick={() => Onreverse(Todo.id)}>
+                      完了リストへ
+                    </button>
+                    <button onClick={() => onDelete(Todo.id)}>削除</button>
+                  </>
+                )}
+              </>
+            );
+          } else if (Head === "完了リスト") {
+            return (
+              <>
+                {Todo.done === true && (
+                  <>
+                    <li key={Todo.id}>{Todo.content}</li>
+                    <button onClick={() => Onreverse(Todo.id)}>
+                      未完了リストへ
+                    </button>
+                    <button onClick={() => onDelete(Todo.id)}>削除</button>
+                  </>
+                )}
+              </>
+            );
+          } else {
+            console.log("エラーが出てる");
+            return null;
+          }
+        })}
       </ul>
     </div>
   );
